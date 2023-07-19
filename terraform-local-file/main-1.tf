b0dfb14abe79e2c0b4a5f5025b5df5d45e0e77e7
mehmetmgrsl <mehmetgursul@gmail.com>
2023-07-19 17:40:03 +0200

    terraform using required_providers

--- terraform-local-file/main-1.tf
+++ terraform-local-file/main-1.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.3.0"
+    }
+  }
+}
+
 resource "local_file" "pet_new" {
     filename = var.filename[count.index]
     content = "We love new pets too!"
