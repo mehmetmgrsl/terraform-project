4eb7d060c0dee8cdc4e72575abe110522232129a
mehmetmgrsl <mehmetgursul@gmail.com>
2023-07-19 22:11:09 +0200

    terraform aws create policy

--- aws/user.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_iam_user" "admin-user" {
-    name = "lucy"
-    tags = {
-        Description ="Tech Team Lead"
-    }
-}
---
+++ aws/users.tf
@@ -0,0 +1,28 @@
+resource "aws_iam_user" "admin-user" {
+    name = "lucy"
+    tags = {
+        Description ="Tech Team Lead"
+    }
+}
+
+resource "aws_iam_policy" "admin-policy" {
+  name = "AdminUsers"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "*",
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "test-attach" {
+  user       = aws_iam_user.admin-user.name
+  policy_arn = aws_iam_policy.admin-policy.arn
+}
