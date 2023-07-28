3a19f2770edd7242ee5b6c9ca019ce75610fecac
mehmetmgrsl <mehmetgursul@gmail.com>
2023-07-28 23:37:38 +0200

    desscription of aws_security_group.ingress has been updated

--- aws/main.tf
+++ aws/main.tf
@@ -95,7 +95,7 @@ resource "aws_security_group" "ssh-access" {
   description = "Allow SSH access from the Internet"
 
   ingress {
-    description      = "TLS from VPC"
+    description      = "SSH"
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
